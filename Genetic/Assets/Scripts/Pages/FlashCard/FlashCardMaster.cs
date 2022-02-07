6f2480bfea920455f2548b6c5a96baa441a981a6
triducnguyen <59855097+triducnguyen@users.noreply.github.com>
2022-02-07 14:57:58 -0700

    demo flash card page
    may modify later

---
+++ Genetic/Assets/Scripts/Pages/FlashCard/FlashCardMaster.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashCardMaster : MonoBehaviour
+{
+    static DBManager manager { get => DBManager.instance; }
+    public Text title;
+    public Text flashCardText;
+
+    //Temp String hold answer
+    private string temp;
+    
+    //Check if answer is revealing
+    private bool isReveal;
+    public Question currentQuestion
+    {
+        get
+        {
+            return _question;
+        }
+        set
+        {
+            _question = value;
+            //set question text
+            flashCardText.text = value.text;
+            temp = GetAnswer().text;
+            
+        }
+    }
+
+    Question _question;
+
+    Answer correctAnswer
+    {
+        get => GetAnswer();
+    }
+
+    Answer GetAnswer()
+    {
+        if (currentQuestion == null)
+        {
+            return null;
+        }
+        else
+        {
+            var aRelation = manager.GetItem<CorrectAnswer>(ca => ca.questionID == currentQuestion.id);
+            return manager.GetItem<Answer>(a => a.id == aRelation.answerID);
+        }
+    }
+    
+    public void RevealAnswer()
+    {
+        string new_title = (!isReveal) ? "Answer: " : "Question: ";
+        string currentText = (!isReveal) ? temp : currentQuestion.text;
+        flashCardText.text = currentText;
+        title.text = new_title;
+        isReveal = !isReveal;
+    }
+
+}
---
+++ Genetic/Assets/Scripts/Pages/FlashCard/FlashCardPage.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashCardPage : PageController
+{
+    static DBManager manager { get => DBManager.instance; }
+    public int numFlashCard;
+    public GameObject flashCardPrefab;
+    public List<Question> questionList;
+    public GameObject contentView;
+
+    private HashSet<Question> questionSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        questionList = new List<Question>();
+        questionSet = new HashSet<Question>();
+
+        for(int i = 0; i < numFlashCard; i++)
+        {
+            Question new_question = GetNewQuestion();
+            if(new_question != null && !questionSet.Contains(new_question))
+            {
+                questionList.Add(new_question);
+                questionSet.Add(new_question);
+                SetNewFlashCard(new_question);
+            }
+        }
+
+        ExpandContenView();
+    }
+
+    public void ExpandContenView()
+    {
+        RectTransform rect = contentView.GetComponent<RectTransform>();
+        if(numFlashCard * 400 > rect.sizeDelta.y)
+        {
+            rect.sizeDelta = new Vector2(rect.sizeDelta.x, (numFlashCard + 1) * 400);
+        }
+    }
+
+    public void SetNewFlashCard(Question question)
+    {
+        if (question == null) return;
+
+        GameObject obj = Instantiate(flashCardPrefab);
+        obj.transform.SetParent(contentView.gameObject.transform);
+        obj.transform.localScale = new Vector3(1, 1, 1);
+        FlashCardMaster flashCard = obj.GetComponent<FlashCardMaster>();
+        flashCard.currentQuestion = question;
+
+    }
+
+    public Question GetNewQuestion()
+    {
+        Question curr = new Question();
+        int count = manager.db.Table<Question>().Count();
+        int id = UnityEngine.Random.Range(1, count + 1);
+        curr = manager.GetItem<Question>(q => q.id == id);
+        return curr;
+    }
+}
